5cfb1161a47b59cadb5d937126f95ef3bed1f46b
MrOty1 <oskaras.venzlauskas@stud.kitm.lt>
2025-01-23 01:17:25 +0200

    updated navbar

--- testas-01.16/script.js
+++ testas-01.16/script.js
@@ -1,10 +1,9 @@
-const rickrollis = (number) => {
-    let rngNumber = (Math.floor(Math.random() * 10));
-    if(rngNumber === 5){
+const easterEgg = () => {
+    let rngNumber = Math.floor(Math.random() * 10);
+    if(rngNumber == Math.floor(Math.random() * 10)){
         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank').focus();
         return
     }else{
         alert("Try Again!")
     }
-    console.log(rngNumber)
 }
